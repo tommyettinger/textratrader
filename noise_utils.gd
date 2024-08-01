9416a660861737c2f1b6b6d347c52336b27ecb3d
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-08-01 04:14:02 -0700

    Still learning the syntax...
    What is with that ternary...

--- noise_utils.gd
+++ noise_utils.gd
@@ -1,15 +1,15 @@
 @tool
 class_name NoiseUtils
 
-func noise1D(x, seed):
-	x += seed * 5.9604644775390625E-8;
-	var xFloor = (x >= 0.0) if int(x) else int(x) - 1
+static func noise1D(x, s):
+	x += s * 5.9604644775390625E-8;
+	var xFloor = int(x) if (x >= 0.0) else int(x) - 1
 	
-	var rise = 1 - ((x >= 0.0 if int(x + x) else int(x + x) - 1) & 2);
+	var rise = 1 - ((int(x + x) if (x >= 0.0) else int(x + x) - 1) & 2);
 	x -= xFloor;
-	var h = ((seed + xFloor ^ 0x7E3779B97F4A7C15) * 0x51B54A32D192ED03) * 1.734723475976807E-18
+	var h = ((s + xFloor ^ -7046029254386353131) * -3335678366873096957) * 1.734723475976807E-18
 	x *= x - 1.0;
 	return rise * x * x * h;
 
-func octaveNoise1D(x, seed):
-	return noise1D(x, seed) * 0.6666666666666666 + noise1D(x * 1.9, ~seed) * 0.3333333333333333;
+static func octaveNoise1D(x, s):
+	return noise1D(x, s) * 0.6666666666666666 + noise1D(x * 1.9, s) * 0.3333333333333333;
