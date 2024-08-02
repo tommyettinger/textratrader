1648bf6c322ac0d60da5fb22b5926bb075df1331
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-08-01 19:38:31 -0700

    Now shr and ushr work.
    Not quickly, of course, but the logic seems right.

--- rich_text_test.gd
+++ rich_text_test.gd
@@ -8,11 +8,18 @@ class_name RichTextTest
 var bbcode = "test"
 
 # Arithmetic shift right.
-static func ashr(n, amt):
+static func shr(n, amt):
 	amt &= 63;
 	return ((n & 0x7FFF_FFFF_FFFF_FFFF) >> amt | (n & ~0x7FFF_FFFF_FFFF_FFFF) / (1 << amt))
 
+# Unsigned (logical) shift right.
+static func ushr(n, amt):
+	amt &= 63;
+	return ((n & 0x7FFF_FFFF_FFFF_FFFF) >> amt | (n & -0x8000_0000_0000_0000) / (-1 << amt))
+	
 func _process_custom_fx(char_fx):
 	char_fx.color = Color.ROYAL_BLUE if \
-	 ashr(-7046029254386353131, 4) == -440376828399147071 else Color.RED
+#	  shr(-7046029254386353131, 4) == -440376828399147071 \
+	  RichTextTest.ushr(-7046029254386353131, 4) == 712544676207699905 \
+	  else Color.RED
 	return true
