34c1694c3dcd65adf5a94622f593498853a8779b
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-08-01 22:52:22 -0700

    Wow, that got complicated.

--- rich_text_test.gd
+++ rich_text_test.gd
@@ -10,7 +10,7 @@ var bbcode = "test"
 # Arithmetic shift right.
 static func shr(n, amt):
 	amt &= 63;
-	return ((n & 0x7FFF_FFFF_FFFF_FFFF) >> amt | (n & ~0x7FFF_FFFF_FFFF_FFFF) / (1 << amt))
+	return ((n & 0x7FFF_FFFF_FFFF_FFFF) >> amt | -((n & -0x8000_0000_0000_0000) / (-1 << amt)))
 
 # Unsigned (logical) shift right.
 static func ushr(n, amt):
@@ -19,7 +19,9 @@ static func ushr(n, amt):
 	
 func _process_custom_fx(char_fx):
 	char_fx.color = Color.ROYAL_BLUE if \
-#	  shr(-7046029254386353131, 4) == -440376828399147071 \
+#	  RichTextTest.shr(-7046029254386353131, 4) == -440376828399147071 \
 	  RichTextTest.ushr(-7046029254386353131, 4) == 712544676207699905 \
+#	  RichTextTest.shr(-7046029254386353131, 63) == -1 \
+#	  RichTextTest.ushr(-7046029254386353131, 63) == 1 \
 	  else Color.RED
 	return true
