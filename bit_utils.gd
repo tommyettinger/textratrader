10dff1f6c69414a51826d5c498299252116265a6
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-08-02 02:43:35 -0700

    These extra things seem to "work".

--- bit_utils.gd
+++ bit_utils.gd
@@ -10,3 +10,22 @@ static func shr(n, amt):
 static func ushr(n, amt):
 	amt &= 63;
 	return ((n & 0x7FFF_FFFF_FFFF_FFFF) >> amt | (n & -0x8000_0000_0000_0000) / (-1 << amt))
+
+# Bitwise left rotation.
+static func rotl(n, amt):
+	amt &= 63;
+	return n << amt | ((n & 0x7FFF_FFFF_FFFF_FFFF) >> 64 - amt | (n & -0x8000_0000_0000_0000) / (-1 << 64 - amt))
+
+# Bitwise right rotation.
+static func rotr(n, amt):
+	amt &= 63;
+	return n << 64 - amt | ((n & 0x7FFF_FFFF_FFFF_FFFF) >> amt | (n & -0x8000_0000_0000_0000) / (-1 << amt))
+
+# Xorshift right.
+static func xsr(n, amt):
+	amt &= 63;
+	return n ^ ((n & 0x7FFF_FFFF_FFFF_FFFF) >> amt | (n & -0x8000_0000_0000_0000) / (-1 << amt))
+
+# Xorshift left.
+static func xsl(n, amt):
+	return n ^ (n << (amt & 63))
