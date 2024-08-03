38c1a7cfa953a930dc4ea9b1e41e77dafd84bf2c
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-08-02 20:27:57 -0700

    Make more octaves an option.
    This also credits Quilez with creating/discovering this type of noise.

--- noise_utils.gd
+++ noise_utils.gd
@@ -2,12 +2,16 @@
 # Written in 2024 by Tommy Ettinger.
 
 ## Tools for 1D noise given a seed.
+##
+## Based on Quilez Basic Noise, [url]https://www.shadertoy.com/view/3sd3Rs[/url].
 @tool
 class_name NoiseUtils
 
-## Gets continuous noise given a distance and seed.
+## Gets continuous 1D noise given a distance and seed.
 ##
 ## x is a 1D distance, and s is any int seed.
+## You could also use [method octave_noise_1d], which calls this more than once
+## to obtain higher quality and less repetitive results.
 static func noise_1d(x: float, s: int) -> float:
 	x += s * 5.9604644775390625E-8;
 	var xFloor = int(x) if (x >= 0.0) else int(x) - 1
@@ -18,10 +22,15 @@ static func noise_1d(x: float, s: int) -> float:
 	x *= x - 1.0;
 	return rise * x * x * h;
 
-## Gets higher-quality continuous noise given a distance and seed.
+## Gets higher-quality continuous 1D noise given a distance and seed.
 ##
 ## x is a 1D distance, and s is any int seed.
-## This uses 2 calls to [method noise_1d] using different frequencies,
-## which are called "octaves."
-static func octave_noise_1d(x: float, s: int) -> float:
-	return noise_1d(x, s) * 0.6666666666666666 + noise_1d(x * 1.9, s) * 0.3333333333333333;
+## This uses 1 or more calls to [method noise_1d] using different frequencies,
+## and amplitudes which are called "octaves."
+static func octave_noise_1d(x: float, s: int, octaves: int = 2) -> float:
+	var noise = 0.0
+	octaves = max(1, octaves)
+	for n in range(octaves - 1, -1, -1):
+		noise += noise_1d(x, s + n) * (1 << n);
+		x *= 1.9
+	return noise / ((1 << octaves) - 1.0)
