02cd8b8cadd4b7ec21a293f26f2f4337b19703ba
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-08-02 21:45:41 -0700

    Add flag option to limit movement past a "pole."
    This also tests the wind effect, and it works.

--- rich_text_wind.gd
+++ rich_text_wind.gd
@@ -4,7 +4,7 @@ class_name RichTextWind
 
 const noise = preload("res://noise_utils.gd")
 
-# Syntax: [wind x=0.8 y=1.5 spacing=1.0 intensity=1.4 seed=0]Wind[/wind]
+# Syntax: [wind x=0.8 y=1.5 spacing=1.0 intensity=1.4 flag=false seed=0]Wind[/wind]
 
 # Define the tag name.
 var bbcode = "wind"
@@ -17,9 +17,10 @@ func _process_custom_fx(char_fx):
 	# Get parameters, or use the provided default value if missing.
 	var x_distance = char_fx.env.get("x", 1.5)
 	var y_distance = char_fx.env.get("y", 1.5)
-	var span = 40
+	var span = 20
 	var spacing = char_fx.env.get("spacing", 1.0)
 	var intensity = char_fx.env.get("intensity", 1.4)
+	var flag = char_fx.env.get("flag", true)
 	var noise_seed = char_fx.env.get("seed", hash(char_fx))
 	
 	var progress = (spacing * char_fx.range.x / float(span))
@@ -27,5 +28,6 @@ func _process_custom_fx(char_fx):
 	var dim = Vector2i(1.5, 1.5)
 	dim = _get_text_server().font_get_glyph_size(char_fx.font, dim, char_fx.glyph_index)
 	char_fx.offset.x = noise.octave_noise_1d(time * x_distance + progress, noise_seed ^ 123456) * dim.x
+	if(flag): char_fx.offset.x = absf(char_fx.offset.x) * sign(x_distance)
 	char_fx.offset.y = noise.octave_noise_1d(time * y_distance + progress + 1.618, noise_seed ^ -123456789) * dim.y
 	return true
