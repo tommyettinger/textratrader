e79ce2b42796f54797591628a78c5ff4ef85f601
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-08-04 21:12:25 -0700

    Mostly working, not sure how...

--- rich_text_squash.gd
+++ rich_text_squash.gd
@@ -18,29 +18,30 @@ func _swingOut(a: float, scale: float = 2.0):
 func _process_custom_fx(char_fx):
 	# Get parameters, or use the provided default value if missing.
 	var span = char_fx.env.get("span", 20)
-	var speed = char_fx.env.get("speed", 3.0)
+	var speed = char_fx.env.get("speed", 0.75)
 	var elastic: bool = char_fx.env.get("elastic", false)
 	if(elastic): span *= 3.0
-	var progress = (char_fx.range.x / float(span))
-	var time = speed * char_fx.elapsed_time - progress
-	var dim = Vector2i(1, 1)
-	dim = _get_text_server().font_get_glyph_size(char_fx.font, dim, char_fx.glyph_index)
+	var progress = (char_fx.relative_index / float(span))
+	var time = speed * (char_fx.elapsed_time - progress)
+	var bd = Vector2i(1, 1)
+	var dim = Vector2(_get_text_server().font_get_glyph_size(char_fx.font, bd, char_fx.glyph_index)) * 0.5
 	if(time < 0.0): 
 		char_fx.visible = false
 	elif (time >= 1.0):
 		char_fx.visible = true
 	elif (time < 0.4):
 		char_fx.visible = true
-		var iv = 1.0 - pow(sin(progress * 1.25 * PI), 2.0) * 0.5
-		char_fx.transform = char_fx.transform.translated_local(dim * Vector2(0.125 - 0.125 * iv, (iv - 1.0) * 0.5))
-		char_fx.transform = char_fx.transform.scaled_local(dim * Vector2(1.0 - iv, iv - 1.0))
+		var iv = (1.0 - pow(sin(time * 1.25 * PI), 2.0)) * 0.5
+		char_fx.transform = char_fx.transform.translated_local(dim * Vector2(0.25 - 0.25 * iv, (iv)))
+		char_fx.transform = char_fx.transform.scaled_local(dim * Vector2(1.0 - iv, iv))
 	else:
 		char_fx.visible = true
-		var iv = (progress - 0.4) * 1.666
+		var iv = (time - 0.4) * 1.666
 		if(elastic):
-			iv = _swingOut(iv) * 0.5 + 0.5
+			iv = _swingOut(iv) * 0.5
 		else:
-			iv = pow(sin(progress * 0.5 * PI), 2.0) * 0.5 + 0.5
-		char_fx.transform = char_fx.transform.translated_local(dim * Vector2(0.125 - 0.125 * iv, (iv - 1.0) * 0.5))
-		char_fx.transform = char_fx.transform.scaled_local(dim * Vector2(1.0 - iv, iv - 1.0))
+			iv = pow(sin(time * 0.5 * PI), 2.0)
+		iv *= 0.5
+		char_fx.transform = char_fx.transform.translated_local(dim * Vector2(0.25 - 0.25 * iv, (iv)))
+		char_fx.transform = char_fx.transform.scaled_local(dim * Vector2(1.0 - iv, iv))
 	return true
